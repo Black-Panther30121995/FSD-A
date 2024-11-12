fcb0551570be4cc065d35444f040dd623f6d8304
Black-Panther30121995 <shivanikumari20050320@gmail.com>
2024-11-12 15:38:51 +0530

    Learning React with Vite

---
+++ Day8-9/react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+//rafce
+
+const App = () => {
+  return (
+    <div id="disp">
+      <h1>Student Details</h1>
+      <h2>Name: Shivani kumari</h2>
+      <h2>Email Id: sk@gmail.com</h2>
+      <h2>Mobile No: 90XXXXXXXX</h2>
+    </div>
+  )
+}
+
+export default App
